40725b6b46a9bf985d1930ecdbe1b4d3b16299a0
Kartik Kumar <me@kartikkumar.com>
2016-07-28 12:18:16 +0300

    Add key wait in bash script and add upload command.

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 # Remove comments, newlines and multiple spaces. Store result in new file.
+echo 'Minifying main file ...'
 grep -o '^[^/]*' OmNomNomFull.java | tr -d '\n' | tr -s ' ' > OmNomNom.java
+
+read -n1 -r -p "Press space to continue..." key
+
+echo 'Compiling ...'
 nxjc OmNomNom.java
+
+read -n1 -r -p "Press space to continue..." key
+
+echo 'Linking ...'
 nxjlink -v -o OmNomNom.nxj OmNomNom
+
+read -n1 -r -p "Press space to continue..." key
+
+echo 'Uploading ...'
+nxjupload OmNomNom.nxj
